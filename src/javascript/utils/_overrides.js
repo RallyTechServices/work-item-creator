ac3364d79d682a142e9ee6c56cea774324d4aab4
arajaramanca <arajaraman@arajaraman-mbr-2.local>
2016-08-19 09:41:54 -0400

    fixing fieldvaluecb issue

--- src/javascript/utils/_overrides.js
+++ src/javascript/utils/_overrides.js
@@ -6,48 +6,49 @@ Ext.override(Rally.ui.combobox.FieldValueComboBox,{
             Ext.Error.raise('field config must be specified when creating a Rally.ui.combobox.FieldValueComboBox');
         }
         this._loadStoreValues();
-    },
-
-    _loadStoreValues: function() {
-            this.field.getAllowedValueStore().load({
-                requester: this,
-                callback: function (records, operation, success) {
-                    var store = this.store;
-                    if (!store) {
-                        return;
-                    }
-                    var noEntryValues = [],
-                        labelValues = _.map(
-                            _.filter(records, this._hasStringValue),
-                            this._convertAllowedValueToLabelValuePair,
-                            this
-                        );
-
-                    if (this.field.getType() === 'boolean') {
-                        labelValues = labelValues.concat([
-                            this._convertToLabelValuePair('Yes', true),
-                            this._convertToLabelValuePair('No', false)
-                        ]);
-                    } else if (this.field.required === false) {
-                        var name = "-- No Entry --",
-                            value = "";
-                        if (this.getUseNullForNoEntryValue()) {
-                            value = null;
-                        }
-                        if (this.field.attributeDefinition.AttributeType.toLowerCase() === 'rating') {
-                            name = this.getRatingNoEntryString();
-                            value = "None";
-                        }
-                        noEntryValues.push(this._convertToLabelValuePair(name, value));
-                    }
-
-                    store.loadRawData(noEntryValues.concat(labelValues));
-                    store.fireEvent('load', store, store.getRange(), success);
-                    this.onReady();
-                },
-                scope: this
-            });
     }
+    // ,
+
+    // _loadStoreValues: function() {
+    //         this.field.getAllowedValueStore().load({
+    //             requester: this,
+    //             callback: function (records, operation, success) {
+    //                 var store = this.store;
+    //                 if (!store) {
+    //                     return;
+    //                 }
+    //                 var noEntryValues = [],
+    //                     labelValues = _.map(
+    //                         _.filter(records, this._hasStringValue),
+    //                         this._convertAllowedValueToLabelValuePair,
+    //                         this
+    //                     );
+
+    //                 if (this.field.getType() === 'boolean') {
+    //                     labelValues = labelValues.concat([
+    //                         this._convertToLabelValuePair('Yes', true),
+    //                         this._convertToLabelValuePair('No', false)
+    //                     ]);
+    //                 } else if (this.field.required === false) {
+    //                     var name = "-- No Entry --",
+    //                         value = "";
+    //                     if (this.getUseNullForNoEntryValue()) {
+    //                         value = null;
+    //                     }
+    //                     if (this.field.attributeDefinition.AttributeType.toLowerCase() === 'rating') {
+    //                         name = this.getRatingNoEntryString();
+    //                         value = "None";
+    //                     }
+    //                     noEntryValues.push(this._convertToLabelValuePair(name, value));
+    //                 }
+
+    //                 store.loadRawData(noEntryValues.concat(labelValues));
+    //                 store.fireEvent('load', store, store.getRange(), success);
+    //                 this.onReady();
+    //             },
+    //             scope: this
+    //         });
+    // }
 });
 
 // overriding to make the store load hidden fields. 
